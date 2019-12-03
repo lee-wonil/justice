6fc9dfa1b80a65e2958080d9673ca0c2f5452664
nnna08 <nnna08@likelion.org>
2019-12-03 17:54:24 +0900

    [news setting]

---
+++ justice/src/project/justice/news/BoardAction.java
@@ -0,0 +1,21 @@
+package project.justice.news;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/news/")
+public class BoardAction {
+
+	
+	@Autowired
+	private NewsDTO dto = null;
+	
+	@RequestMapping("news_list.ju")
+	public String news_list(NewsDTO dto) {
+		return "news_list";
+	}
+	
+	
+}
---
+++ justice/src/project/justice/news/NewsDTO.java
@@ -0,0 +1,59 @@
+package project.justice.news;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewsDTO {
+
+	private String news_url;
+	private String news_date;
+	private String news_field;
+	private String news_keyword;
+	private ArrayList news_connectionword;
+	private String news_press;
+	private String news_reporter;
+	
+	
+	public String getNews_url() {
+		return news_url;
+	}
+	public void setNews_url(String news_url) {
+		this.news_url = news_url;
+	}
+	public String getNews_date() {
+		return news_date;
+	}
+	public void setNews_date(String news_date) {
+		this.news_date = news_date;
+	}
+	public String getNews_field() {
+		return news_field;
+	}
+	public void setNews_field(String news_field) {
+		this.news_field = news_field;
+	}
+	public String getNews_keyword() {
+		return news_keyword;
+	}
+	public void setNews_keyword(String news_keyword) {
+		this.news_keyword = news_keyword;
+	}
+	public ArrayList getNews_connectionword() {
+		return news_connectionword;
+	}
+	public void setNews_connectionword(ArrayList news_connectionword) {
+		this.news_connectionword = news_connectionword;
+	}
+	public String getNews_press() {
+		return news_press;
+	}
+	public void setNews_press(String news_press) {
+		this.news_press = news_press;
+	}
+	public String getNews_reporter() {
+		return news_reporter;
+	}
+	public void setNews_reporter(String news_reporter) {
+		this.news_reporter = news_reporter;
+	}
+
+}
