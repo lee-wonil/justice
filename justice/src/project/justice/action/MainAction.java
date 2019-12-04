b118332437dd72229e1efbd9303b9276c8bca998
pc <pc@pc-PC>
2019-12-04 12:15:46 +0900

    로그인

--- justice/src/project/justice/action/MainAction.java
+++ justice/src/project/justice/action/MainAction.java
@@ -1,6 +1,7 @@
 package project.justice.action;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 //test
 @Controller
@@ -9,5 +10,19 @@ public class MainAction {
 	@RequestMapping("main.ju")
 	public String main() {
 		return "main";
+	}
+	@RequestMapping("login.ju")
+	public String login() {		
+		return "login";
+	}
+	@RequestMapping("loginPro.ju")
+	public String loginPro(String id,String passwd,Model model) {
+		model.addAttribute("id", id);
+		model.addAttribute("passwd", passwd);
+		return "loginPro";
+	}
+	@RequestMapping("registForm.ju")
+	public String rigistForm() {
+		return "registForm";
 	}
 }
